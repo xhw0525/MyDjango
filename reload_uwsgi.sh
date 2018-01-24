5be186cdb360a106f9367c2fe43e74b00be82723
xhw <1982700252@qq.com>
2018-01-24 16:16:05 +0800

    5

--- reload_uwsgi.sh
+++ reload_uwsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NAME="walkerfree"
+NAME="MyDjango_uwsgi"
 if [ ! -n "$NAME" ];then
     echo "no arguments"
     exit;
@@ -8,11 +8,11 @@ fi
 echo $NAME
 ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
 echo $ID
-echo "################################################"
+echo "此处是不是该输出点什么"
 for id in $ID
 do
 kill -9 $id
 echo "kill $id"
 done
-echo  "################################################"
+echo  "此处是不是该输出点什么"
 uwsgi --ini /home/uxhw/MyDjango/MyDjango_uwsgi.ini
